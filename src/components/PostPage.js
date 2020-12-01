2b966bba58eada9401fe25805444993a12855239
Matt Haffner <mrhaffner13@gmail.com>
2020-11-30 21:18:28 -0600

    created a function that properly creates comments in MongoDB in PostPage object

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState } from 'react';
 import { useParams } from "react-router-dom";
 
 const PostPage = (props) => {
     const { posts } = props;
     let params = useParams()
-
     const post = posts.find((obj) => {
         return obj.hyphenTitle === params.hyphenTitle
-    })
+    });
+
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log(JSON.stringify({ author, text }))
+        try {
+            const response = await fetch(`http://localhost:3000/blog/${post['_id']}/comment`, {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ author, text }),
+            })
+            const data = await response
+            console.log(data)
+        } catch (err) {
+            alert(err) //change for production, don't want an alert
+        }
+    };
 
     return (
         <div>
@@ -19,12 +40,12 @@ const PostPage = (props) => {
             </div>
             <div>
                 <h3>Comments</h3>
-                <form action="POST">
+                <form action="">
                     <label htmlFor="author">Name</label><br/>
-                    <input type="text"/><br/>
+                    <input type="text" onChange={(e) => setAuthor(e.target.value)} /><br/>
                     <label htmlFor="text">Message</label><br/>
-                    <textarea name="" id="" cols="30" rows="10"></textarea><br/>
-                    <button>Submit Comment</button>
+                    <textarea name="" id="" cols="30" rows="10" onChange={(e) => setText(e.target.value)} /><br/>
+                    <button onClick={handleSubmit} >Submit Comment</button>
                 </form>
             </div>
         </div>
